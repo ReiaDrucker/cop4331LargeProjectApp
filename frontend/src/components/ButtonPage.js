61b0f755c2b0f9ffbff042f4133ab1b8686bdad2
Mary McIntosh <mgmcintosh123@gmail.com>
2020-07-21 14:14:07 -0400

    fix-4

--- frontend/src/components/ButtonPage.js
+++ frontend/src/components/ButtonPage.js
@@ -6,40 +6,39 @@ const BASE_URL = 'https://cop4331-g25-app.herokuapp.com/';
 
 function ButtonPage() {
 
-}
+    // swap to login
+    const gotoLogin = event => {
+        event.preventDefault();
 
-// swap to login
-const gotoLogin = event => {
-    event.preventDefault();
+        document.getElementById("loginDiv").style.display = "block";
+        document.getElementById("buttonsDiv").style.display = "none";
+    }
 
-    document.getElementById("loginDiv").style.display = "block";
-    document.getElementById("buttonsDiv").style.display = "none";
-}
+    // swap to signup
+    const gotoSignup = event => {
+        event.preventDefault();
 
-// swap to signup
-const gotoSignup = event => {
-    event.preventDefault();
+        document.getElementById("signupDiv").style.display = "block";
+        document.getElementById("buttonsDiv").style.display = "none";
+    }
 
-    document.getElementById("signupDiv").style.display = "block";
-    document.getElementById("buttonsDiv").style.display = "none";
-}
+    // swap to forgot PW
+    const gotoForgotPW = event => {
+        event.preventDefault();
 
-// swap to forgot PW
-const gotoForgotPW = event => {
-    event.preventDefault();
-
-    document.getElementById("ForgotDiv").style.display = "block";
-    document.getElementById("buttonsDiv").style.display = "none";
-}
+        document.getElementById("ForgotDiv").style.display = "block";
+        document.getElementById("buttonsDiv").style.display = "none";
+    }
 
 
-return (
-    <div id="buttonsDiv">
-        <button type="button" id="sGoToLogin" class="buttons" onClick={gotoLogin}> Login </button><br />
-        <button type="button" id="sGoToSignup" class="buttons" onClick={gotoSignup}> Signup </button><br />
-        <button type="button" id="sGoToForgotPW" class="buttons" onClick={gotoForgotPW}> Forgot Password </button>
-    </div>
+    return (
+        <div id="buttonsDiv">
+            <button type="button" id="sGoToLogin" class="buttons" onClick={gotoLogin}> Login </button><br />
+            <button type="button" id="sGoToSignup" class="buttons" onClick={gotoSignup}> Signup </button><br />
+            <button type="button" id="sGoToForgotPW" class="buttons" onClick={gotoForgotPW}> Forgot Password </button>
+        </div>
 
-);
+    );
+};
 
 export default ButtonPage;
